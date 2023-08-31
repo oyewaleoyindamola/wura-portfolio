15da0f436108177b5257a4801ed084fc84d66276
oyewaleoyindamola <oyewaleoyindamola@gmail.com>
2023-08-31 17:48:47 +0100

    updatessss

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
       id="home"
       data-aos="fade-up"
     >
-      <div className="absolute inset-0 3xl:top-[120px] xl:mt-36 px-5 lg:px-0 max-w-7xl justify-between mx-auto flex flex-wrap items-start gap-5">
+      <div className="absolute inset-0 3xl:top-[120px] xl:mt-36 px-5 lg:px-0 max-w-7xl justify-between mx-auto flex flex-wrap items-start gap-0">
         <section className="text-[2rem] lg:text-[3.6rem] font-bold text-[#f5f5f5] font-[poppins-extrabold]">
           Hi 👋, <br /> My name is <br />
           <span className="name">Oyewale Oyindamola W.</span>
